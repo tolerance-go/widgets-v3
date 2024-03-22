75709a724c5d7187d126e392d4964ed4fea89bf9
yarnb <yarnb@foxmail.com>
2024-03-22 19:38:58 +0800

    docs(SearchTable): 新增查看 demo

--- src/SearchTable/demos/basic.tsx
+++ src/SearchTable/demos/basic.tsx
@@ -1,6 +1,6 @@
-import { Cascader, Form, Icon, Input, Tooltip } from 'antd';
+import { Button, Cascader, Form, Icon, Input, Tooltip } from 'antd';
 import React from 'react';
-import { SearchTable } from 'widgets-v3';
+import { DrawerForm, ProDescriptions, SearchTable } from 'widgets-v3';
 import delay from 'delay';
 
 const residences = [
@@ -38,32 +38,127 @@ const residences = [
   },
 ];
 
-const columns = [
-  {
-    title: '姓名',
-    dataIndex: 'name',
-    key: 'name',
-    filters: [
-      { text: 'Joe', value: 'Joe' },
-      { text: 'Jim', value: 'Jim' },
-    ],
-    sorter: true,
-  },
-  {
-    title: '年龄',
-    dataIndex: 'age',
-    key: 'age',
-  },
-  {
-    title: '住址',
-    dataIndex: 'address',
-    key: 'address',
-  },
-];
-
 export default () => (
   <SearchTable
-    columns={columns}
+    columns={[
+      {
+        title: '姓名',
+        dataIndex: 'name',
+        key: 'name',
+        filters: [
+          { text: 'Joe', value: 'Joe' },
+          { text: 'Jim', value: 'Jim' },
+        ],
+        sorter: true,
+      },
+      {
+        title: '年龄',
+        dataIndex: 'age',
+        key: 'age',
+      },
+      {
+        title: '住址',
+        dataIndex: 'address',
+        key: 'address',
+      },
+      {
+        title: '操作',
+        align: 'center',
+        render: () => {
+          return (
+            <span>
+              <DrawerForm
+                title="查看"
+                trigger={<Button type="link">查看</Button>}
+                renderFormItems={() => {
+                  return (
+                    <ProDescriptions
+                      request={async () => {
+                        await delay(1000);
+                        return {
+                          email: 'yarnb@qq.com',
+                          password: 123,
+                          nickname: 'yarnb',
+                        };
+                      }}
+                    >
+                      {(data) => {
+                        return {
+                          type: 'Tabs',
+                          props: {},
+                          children: [
+                            {
+                              type: 'TabPane',
+                              props: { tab: 'Tab 1' },
+                              children: [
+                                {
+                                  type: 'Card',
+                                  props: { title: 'Card Title 1' },
+                                  children: [
+                                    {
+                                      type: 'Descriptions',
+                                      props: { title: 'User Info 1' },
+                                      children: [
+                                        {
+                                          type: 'DescriptionsItem',
+                                          props: { label: 'Name', children: 'John Doe' },
+                                        },
+                                        {
+                                          type: 'DescriptionsItem',
+                                          props: { label: 'Age', children: '30' },
+                                        },
+                                        {
+                                          type: 'DescriptionsItem',
+                                          props: { label: 'Country', children: 'USA' },
+                                        },
+                                      ],
+                                    },
+                                  ],
+                                },
+                              ],
+                            },
+                            {
+                              type: 'TabPane',
+                              props: { tab: 'Tab 2' },
+                              children: [
+                                {
+                                  type: 'Card',
+                                  props: { title: 'Card Title 2' },
+                                  children: [
+                                    {
+                                      type: 'Descriptions',
+                                      props: { title: 'User Info 2' },
+                                      children: [
+                                        {
+                                          type: 'DescriptionsItem',
+                                          props: { label: 'Name', children: 'Jane Doe' },
+                                        },
+                                        {
+                                          type: 'DescriptionsItem',
+                                          props: { label: 'Age', children: '28' },
+                                        },
+                                        {
+                                          type: 'DescriptionsItem',
+                                          props: { label: 'Country', children: 'Canada' },
+                                        },
+                                      ],
+                                    },
+                                  ],
+                                },
+                              ],
+                            },
+                          ],
+                        };
+                      }}
+                    </ProDescriptions>
+                  );
+                }}
+              ></DrawerForm>
+            </span>
+          );
+        },
+      },
+    ]}
     searchForm={{
       renderFormItems: ({ form: { getFieldDecorator } }) => {
         return [
