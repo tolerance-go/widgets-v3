b0fc75cfab0772afc00ffee021da94b76a167702
yarnb <yarnb@foxmail.com>
2024-04-03 23:15:20 +0800

    feat(Store): 支持嵌套功能

--- src/Store/index.md
+++ src/Store/index.md
@@ -8,3 +8,10 @@ group:
 ## 基本使用
 
 <code src="./demos/requestData" />
+
+
+## 嵌套使用
+
+为了增加易用性，如果不指定 name 那么子函数节点注入的就是最近的整个 store
+
+<code src="./demos/nested" />
--- src/Store/index.tsx
+++ src/Store/index.tsx
@@ -1,16 +1,21 @@
-import React, { forwardRef, useLayoutEffect, useRef, useState } from 'react';
-import DialogForm, { DialogFormBaseProps } from '../DialogForm';
+import React, { createContext, useContext, useEffect, useMemo, useRef, useState } from 'react';
 import { Spin, message } from 'antd';
 
 type StoreProps = {
   children?: (data: Record<string, any>) => React.ReactNode;
   request?: () => Promise<Record<string, any>>;
+  name?: string; // 新增属性，用于标识每个Store
 };
 
-const Store = ({ children, request }: StoreProps) => {
+export const StoreContext = createContext<Record<string, any>>({});
+
+const Store = ({ children, request, name }: StoreProps) => {
   const [loading, setLoading] = useState(false);
   const [data, setData] = useState<Record<string, any>>({});
-  const requestCounterRef = useRef(0); // 请求计数器
+  const requestCounterRef = useRef(0);
+
+  // 使用 useContext 获取上级 Store 提供的数据
+  const parentData = useContext(StoreContext);
 
   const fetch = async () => {
     if (!request) return;
@@ -24,16 +29,11 @@ const Store = ({ children, request }: StoreProps) => {
       }
     } catch (error) {
       let errorMessage;
-
-      // 判断错误的类型
       if (typeof error === 'string') {
-        // 如果错误是一个字符串
         errorMessage = error;
       } else if (error instanceof Error) {
-        // 如果错误是Error对象
         errorMessage = error.message;
       }
-
       console.log(error);
       message.error(errorMessage || '请求视图数据失败');
     } finally {
@@ -43,11 +43,20 @@ const Store = ({ children, request }: StoreProps) => {
     }
   };
 
-  useLayoutEffect(() => {
+  useEffect(() => {
     fetch();
   }, []);
 
-  return <Spin spinning={loading}>{children?.(data)}</Spin>;
+  // 提供更新后的数据给子组件
+  const value = useMemo(() => {
+    return name ? { ...parentData, [name]: data } : data;
+  }, [name, parentData, data]);
+
+  return (
+    <StoreContext.Provider value={value}>
+      <Spin spinning={loading}>{children?.(value)}</Spin>
+    </StoreContext.Provider>
+  );
 };
 
 export default Store;
