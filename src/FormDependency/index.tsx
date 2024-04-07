5064c7785ba7bd73ef055ee62d716dc88384d6de
yarnb <yarnb@qq.com>
2024-04-07 10:43:03 +0800

    docs(FormDependency): 新增注释

--- src/FormDependency/index.tsx
+++ src/FormDependency/index.tsx
@@ -17,6 +17,10 @@ const FormDependency = ({ children }: FormDependencyProps) => {
   useEffect(() => {
     if (!formEventBus) return;
 
+    /**
+     * 目前的策略是 Form 融合嵌套，Form 自身不作为一个单独的 FormField，而只要在表单字段触发修改的时候
+     * 才导致 form 改变，而类似 TabsForm 的布局改变的时候，需要通过事件方式通知
+     */
     const handler = formEventBus.on('onTabsFormTabItemsChange', () => {
       setFormValues(form?.getFieldsValue() ?? {});
     });
