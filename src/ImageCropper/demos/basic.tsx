e86a3bd64a9cc65d3ee95008b96bd30871d41d75
yarnb <yarnb@qq.com>
2024-04-08 18:00:37 +0800

    chore: 暂时提交

---
+++ src/ImageCropper/demos/basic.tsx
@@ -0,0 +1,5 @@
+import { Button, Cascader, Form, Icon, Input, Tooltip } from 'antd';
+import React from 'react';
+import { ImageCropper, ModalForm } from 'widgets-v3';
+
+export default () => <ImageCropper />;
